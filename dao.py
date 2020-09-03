027c89b4fdbaa7aa184d764e5e35b58cfbd7476f
Zhi Chen <chenzhi.james@gmail.com>
2020-09-03 10:05:22 +0800

    Update dao.py

--- dao.py
+++ dao.py
@@ -1,7 +1,6 @@
 print('The DAO of ROAD: ')
 print('ROME was not built in one day. All ROADs lead to ROME')
-print('All ROADs lead to ROME.')
-print('Matlab is RSOME, ROAD in Python is more than RSOME!')
+print('Matlab is RSOME. ROAD in Python is more than RSOME!')
 print('The ROAD in Python is more than RSOME!')
 
 print('\n')
